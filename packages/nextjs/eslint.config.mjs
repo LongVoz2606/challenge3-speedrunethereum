efad01254e52f7b033584685edf6b31e6e68d7c3
LongVoz2606 <168708715+LongVoz2606@users.noreply.github.com>
2025-12-31 17:00:34 +0700

    challenge 3

--- packages/nextjs/eslint.config.mjs
+++ packages/nextjs/eslint.config.mjs
@@ -21,7 +21,7 @@ export default defineConfig([
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/ban-ts-comment": "off",
       "@typescript-eslint/no-unused-vars": "warn",
-      "@typescript-eslint/no-unused-expressions": "expressions",
+      "@typescript-eslint/no-unused-expressions": "off",
       "prettier/prettier": [
         "warn",
         {
